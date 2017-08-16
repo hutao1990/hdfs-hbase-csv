79ffb3c5a8610efe33ff39974148ddf28e0c80c0
hutao <hutao@gomeplus.com>
2017-08-16 17:50:02 +0800

    hive to hbase

--- src/main/scala/com/gome/dataimport/main/ImportData.scala
+++ src/main/scala/com/gome/dataimport/main/ImportData.scala
@@ -2,8 +2,6 @@ package com.gome.dataimport.main
 
 import com.gome.dataimport.utils.{HbaseUtil, SparkUtils}
 import org.apache.hadoop.hbase.client.{Connection, Put}
-import org.apache.hadoop.hbase.util.FSUtils
-import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.types.StructType
 import org.apache.spark.sql.{DataFrame, Row, SQLContext}
 
@@ -22,7 +20,7 @@ object ImportData {
         "<data file>          csv file\n" +
         "<table string>       hbase table\n" +
         "<family string>      hbase family\n" +
-        "<key schema string>  column as hbase rowkey")
+        "<key schema string>  column as hbase rowkey\n")
       System.exit(1)
     }
 
@@ -33,23 +31,24 @@ object ImportData {
     val sqlContext: SQLContext = SparkUtils.createSqlContext(Map[String, String](), "hdfs-to-hbase")
 
     //文件异常行处理
-    sqlContext.sparkContext.textFile(args(1)).map(line => line.replace("\'", "").replaceAll(",,", ",")).saveAsTextFile(args(1) + ".format")
+//    sqlContext.sparkContext.textFile(args(1)).map(line => line.replace("\'", "").replaceAll(",,", ",")).saveAsTextFile(args(1) + ".format")
 
 
     import com.databricks.spark.csv._
     // schema 文件读取
-    val sch: DataFrame = sqlContext.csvFile(args(0), useHeader = true, quote = '\'')
+    val sch: DataFrame = sqlContext.csvFile(args(0), useHeader = true)
     val schema: StructType = sch.schema
-    val columns: Seq[String] = schema.map(s => s.name).filter(name => !name.toUpperCase.equals("HASHED"))
-    val map = Map[String, List[String]](args(3) -> columns.toList)
+    val columns: Seq[String] = schema.map(s => s.name).filter(name => !name.toUpperCase.equals("KEY"))
+                                                      .filter(name => !name.toUpperCase.equals("OPR_TIME"))
+    val map = Map[String, List[String]](args(3) -> columns.toList,"family"->List("CURRENT_TS"))
 
     // 数据文件读取
-    val frame: DataFrame = sqlContext.csvFile(args(1) + ".format", useHeader = false, quote = '\'', ignoreLeadingWhiteSpace = true, ignoreTrailingWhiteSpace = true)
+    val frame: DataFrame = sqlContext.csvFile(args(1), useHeader = false,delimiter = '~', ignoreLeadingWhiteSpace = true, ignoreTrailingWhiteSpace = true)
     val frame1: DataFrame = sqlContext.createDataFrame(frame.map(r => r), schema)
     frame1.registerTempTable("tempTable")
 
     //生成最终DF
-    val results: DataFrame = sqlContext.sql("select " + args(4) + " as key," + columns.mkString(",") + " from tempTable")
+    val results: DataFrame = sqlContext.sql("select " + args(4) + " as key," + columns.mkString(",") + ",from_unixtime(unix_timestamp(),'yyyy-MM-dd HH:mm:ss.SSS') as CURRENT_TS from tempTable")
 
     //插入HBASE
     results.foreachPartition(partition => {
@@ -59,7 +58,7 @@ object ImportData {
         rows.foreach(row => {
           buffer += HbaseUtil.createPuts(row.mkString(",").split(",").map(x => x.replace("NULL", "")), map)(1)
         })
-        //        println(buffer)
+//                println(buffer)
         HbaseUtil.add(connection, args(2), buffer.toList)
       })
       HbaseUtil.close(connection)
